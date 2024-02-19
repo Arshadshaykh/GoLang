ecef89115c41c94b71ce922ede2313a0da8ddd82
Arshad Shaikh <105237701+Arshadshaykh@users.noreply.github.com>
2024-02-19 18:14:34 +0530

    office learning

---
+++ first/greetings.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func greetUser() {
+	fmt.Println("hello there user")
+}
---
+++ first/home.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var name = "Arshad"
+	fmt.Println(name)
+	fmt.Println("Hello, World!")
+}
