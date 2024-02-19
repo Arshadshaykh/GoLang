3e8d0e6fad3aa3003e31e5373424faa40f76814f
Arshad Shaikh <arshaikh382002@gmail.com>
2024-02-19 23:33:56 +0530

    conditions

---
+++ conditions/conditions.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	name := 6
+	if name == 6 {
+		fmt.Println("number is smaller")
+	} else {
+		fmt.Println("Number is larger")
+	}
+
+}
---
+++ conditions/go.mod
@@ -0,0 +1,3 @@
+module example.com/condition
+
+go 1.22.0
